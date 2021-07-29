cb44cab08a1b2371dccc6eae85a8ffbd1fafc6a0
chiru7cj <chiranjeev.kumar@knoldus.com>
2021-07-29 20:38:46 +0530

    First commit

---
+++ Assign/string-api/src/main/java/stringFunction.java
@@ -0,0 +1,12 @@
+public class stringFunction {
+    public  static String  reverseString(String args)
+    {   StringBuffer sbr = new StringBuffer(args);
+        sbr.reverse(); // To reverse the string
+        return  sbr.toString();
+    }
+    public  static int  sizeOfString(String args)
+    {
+        return  args.length();
+    }
+
+}
---
+++ Assign/string-impl/src/main/java/Application.java
@@ -0,0 +1,15 @@
+
+public class Application {
+
+    public static void main(String agrs[])
+
+    {
+  String str="Maven Assignment";
+      String reverse=  stringFunction.reverseString(str);
+       int stringlength= stringFunction.sizeOfString(str);
+        System.out.println(reverse);
+        System.out.println(stringlength);
+
+
+    }
+}
